d67abdf1e573f3b51ce0549cd67b49b2617bb195
janferech <jan.trautwein11@gmail.com>
2024-10-11 09:13:08 +0200

    delete too simple code

--- Beginnercode/Aufgabe1_distanz.jl
+++
@@ -1,9 +0,0 @@
-function calcDistance(x1, x2, y1, y2)
-    sqrt((x2 - x1)^2 + (y2 - y1)^2)
-end
-
-function printDistance()
-    println(calcDistance(0, 3, 0, 4)) 
-end
-
-printDistance()
--- Beginnercode/Fakultaet.jl
+++
@@ -1,8 +0,0 @@
-function find_fakultaet(n)
-    fakultaet = 0
-    if n <= 0
-        println(fakultaet)
-    else
-        fakultaet = n * (find_fakultaet(n-1))
-    end 
-end
